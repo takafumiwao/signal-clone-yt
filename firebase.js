e526847adebda979b2cdbe6fcaa2c8556ad3da81
iwaotakashi <37037413+takafumiwao@users.noreply.github.com>
2021-12-26 12:52:47 +0900

    Building the LoginScreen

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDxjdoIe3pR-8zEucGs8qXnkZugvv9fkXs",
+    authDomain: "signal-clone-yt-build-1c65c.firebaseapp.com",
+    projectId: "signal-clone-yt-build-1c65c",
+    storageBucket: "signal-clone-yt-build-1c65c.appspot.com",
+    messagingSenderId: "301115041724",
+    appId: "1:301115041724:web:961a2323dcfb2cbef56db9"
+  };
